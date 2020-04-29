942640e51fe1b71cf9b94bba6f06ad46e66f4cc0
Geoff Speirs <geoff.speirs@myob.com>
2020-04-30 00:24:19 +1000

    refactor

--- kata-magic-year-calculator/MagicYearCalculator.cs
+++ kata-magic-year-calculator/MagicYearCalculator.cs
@@ -8,12 +8,13 @@ namespace MagicYearCalculator
         {
 
             switch(command){
-                case ShowWelcomeMessageAndPromptForName showWelcomeMessageAndPromptForName:
+                case ShowWelcomeMessageAndPromptForName showWelcomeMessageAndPromptForName: 
+                    return ProcessPromptNameCommand(input);
                 case RepromptForName repromptForName: 
                     return ProcessPromptNameCommand(input);
-                case PromptForSurname promptForSurname:
+                case PromptForSurname promptForSurname: 
                     return ProcessPromptSurnameCommand(promptForSurname, input);
-                case RepromptForSurname repromptForSurname:
+                case RepromptForSurname repromptForSurname: 
                     return ProcessPrepromptSurnameCommand(repromptForSurname, input);
                 default: 
                     throw new ArgumentException();
@@ -21,24 +22,15 @@ namespace MagicYearCalculator
         }
 
         private static ICommand ProcessPromptNameCommand(string input){
-            switch(input){
-                case "": return new RepromptForName();
-                default: return new PromptForSurname(input);
-            }   
+            if (input == "") return new RepromptForName(); else return new PromptForSurname(input);
         }
 
         private static ICommand ProcessPromptSurnameCommand(PromptForSurname command, string input){
-            switch(input){
-                case "": return new RepromptForSurname(command.name);
-                default: return new PromptForAnnualSalary(command.name, input);
-            }   
+            if (input == "") return new RepromptForSurname(command.name); else return new PromptForAnnualSalary(command.name, input);
         }
 
         private static ICommand ProcessPrepromptSurnameCommand(RepromptForSurname command, string input){
-            switch(input){
-                case "": return new RepromptForSurname(command.name);
-                default: return new PromptForAnnualSalary(command.name, input);
-            }   
+            if (input == "") return new RepromptForSurname(command.name); else return new PromptForAnnualSalary(command.name, input);
         }        
     }
 }
